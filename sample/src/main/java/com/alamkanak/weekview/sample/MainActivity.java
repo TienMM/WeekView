269b02fd368584bbd8e352f5abbccf827045feb9
baddeverVN <minhtien1107vn@gmail.com>
2016-11-08 07:21:44 +0700

    1.0

---
+++ sample/src/main/java/com/alamkanak/weekview/sample/MainActivity.java
@@ -0,0 +1,32 @@
+package com.alamkanak.weekview.sample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonBasic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BasicActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.buttonAsynchronous).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AsynchronousActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
